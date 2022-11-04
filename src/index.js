da6550afc923bbda1af39e9645d3bca3abcffb3b
Dina Spencer <dkay22@gmail.com>
2022-11-04 17:39:54 +0100

    Tweak browserRouter and package.json

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import Gerund from './components/Gerund';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename="/englishapp">
   <Routes>
     <Route path="/" element={ <App />} />
     <Route path="/taboo" element={<Taboo />} />

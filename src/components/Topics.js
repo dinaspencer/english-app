fd22831def1cc28a97cf2cf74095e0604f750e14
Dina Spencer <46998017+dinaspencer@users.noreply.github.com>
2025-02-18 12:33:45 +0100

    update topics

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -45,7 +45,7 @@ const TopicCard = ({ topiccard }) => {
   }
 
  const topic_cards = [
-  {"id": 1, "topic": "fitfluencers / influencers"},  
+  {"id": 1, "topic": "influencers"},  
   {"id": 2, "topic": "chatGPT"}, 
   {"id": 3, "topic": "a TV series I enjoyed"}, 
   {"id": 4, "topic": "something worth investing in"},
@@ -53,23 +53,24 @@ const TopicCard = ({ topiccard }) => {
   {"id": 6, "topic": "a guilty pleasure"}, 
   {"id": 7, "topic": "global warming"},
   {"id": 8, "topic": "the best way to save or invest money"},
-  {"id": 9, "topic": "the Spanish monarchy"},
+  {"id": 9, "topic": "the monarchy"},
   {"id": 11, "topic": "immigration"},
   {"id": 12, "topic": "social media"},
   {"id": 13, "topic": "my dream house"},
-  {"id": 14, "topic": "the US president"},
+  {"id": 14, "topic": "current events and the state of the world"},
   {"id": 15, "topic": "a great book I read"},
   {"id": 16, "topic": "something that should be banned"},
-  {"id": 17, "topic": "three cities I would love to visit"},
+  {"id": 17, "topic": "three countries I would love to visit"},
   {"id": 19, "topic": "something I'm eager to do"},
   {"id": 20, "topic": "inflation"},
   {"id": 21, "topic": "TikTok and other Chinese tech companies"},
   {"id": 22, "topic": "something I can't live without"},
   {"id": 23, "topic": "online/app dating"},
-  {"id": 24, "topic": "Nadal or Alcaraz?"},
+  {"id": 24, "topic": "a hot take (a candid and possibly unpopular opinion of mine)"},
   {"id": 25, "topic": "a dealbreaker for you in relationships"},
   {"id": 26, "topic": "if I ran the world..."},
-  {"id": 27, "topic": "I try to steer clear of...."}
+  {"id": 27, "topic": "I try to steer clear of...."},
+  {"id": 28, "topic": "an activity that makes me lose track of time"}
 ]
 
      const shuffleCards = topic_cards.sort(() => Math.random() - 0.5);
